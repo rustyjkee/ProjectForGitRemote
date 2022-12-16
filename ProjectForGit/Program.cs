e60d73fc1b8a97328643222f1b577c78d7cf5bdd
Josef Dzeranov <iodzeranov@mail.ru>
2022-12-16 16:07:51 +0300

    Убрал лишнее

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -6,12 +6,7 @@ namespace ProjectForGit
     {
         static void Main(string[] args)
         {
-            // изменение для подзадачи1
             Console.WriteLine("Hello World!");
-            // изменение для подзадачи2
-            // изменение для подзадачи3
-            // изменение для подзадачи4
-            // изменение для подзадачи5
         }
     }
 }
