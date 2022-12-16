3efc5c576224eaeea9935dfc0b2351559f13c5a3
Josef Dzeranov <iodzeranov@mail.ru>
2022-12-16 16:19:11 +0300

    сделали снова изменения не в той ветке

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -7,6 +7,8 @@ namespace ProjectForGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // сделали снова изменения не в той ветке
         }
     }
 }
