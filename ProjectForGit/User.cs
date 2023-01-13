8dd8db6c6f1fd2d4d1f50d663113013e68007497
Josef Dzeranov <iodzeranov@mail.ru>
2023-01-13 10:25:35 +0300

    +

--- ProjectForGit/User.cs
+++ ProjectForGit/User.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectForGit
+﻿namespace ProjectForGit
 {
     internal class User
     {
-        public User()
-        {
-
-        }
     }
 }
